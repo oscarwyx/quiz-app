412ca3419addd343412cb87febf828ddba5a2ff0
oscarwyx <oscarwyx@udel.edu>
2021-09-23 20:33:26 -0400

    update

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -9,14 +9,16 @@ class Counters extends Component {
             {id: 3, value: 0},
             {id: 4, value: 0}
         ]
-    }
+    };
+
+    handleDelete =()=>{
+        console.log("Event Handler Called");
+    };
     render() { 
         return(
         <div>
             { this.state.counters.map(counter => (
-                <Counter key={counter.id} value={counter.value}>
-                    <h4>Title</h4>
-                </Counter>
+                <Counter key={counter.id} onDelete= {this.handleDelete}  value={counter.value} id={counter.id} />
             ))}
         </div>
     );
