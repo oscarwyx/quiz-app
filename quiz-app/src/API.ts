dc4a365a28d18175539eb9d6f642470556dd2637
oscarwyx <oscarwyx@udel.edu>
2021-09-28 22:40:55 -0400

    update

--- quiz-app/src/API.ts
+++ quiz-app/src/API.ts
@@ -1,9 +1,9 @@
-
+import {shuffleArray} from './utils';
 export type Question = {
     category: string;
     correct_answer: string;
     difficulty: string;
-    incorrect_answer: string[];
+    incorrect_answers: string[];
     question: string;
     type: string;
 }
@@ -21,5 +21,11 @@ export enum Difficulty {
  export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<[]> => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json();
-    console.log(data);
+    return data.results.map((question: Question)=>(
+        {
+            ...question,
+            answers: shuffleArray([...question.incorrect_answers,question.correct_answer])
+            
+        }
+    ))
  }
